fabb5c40ee42597848533edd0a58a37a027f4d5d
[Jmml14] <[juanmontes44@gmail.com]>
2023-05-29 21:48:40 +0200

    igual

--- Trabajo/Run1D.py
+++ Trabajo/Run1D.py
@@ -1,30 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 import PML1D
+import OBSTACLE1D
 
 fd = PML1D.FDTD_PML1D()
 
-eps = 1.0
-mu = 1.0
-c = 1/np.sqrt(eps*mu)
+eps = 1.0                   # Permitividad eléctrica
+mu = 1.0                    # Permeabilidad magnética
+c = 1/np.sqrt(eps*mu)       # Velocidad de la onda
 
-# CONDICIÓN INICAL
+# CONDICIÓN INICAL gaussiana
 
-x_0 = 2*fd.L/3
-s_0 = 0.25
+x_0 = 1*fd.L/3  # Centro de la gaussiana
+s_0 = 0.25      # anchura de la gaussiana
 
 e_ini = np.exp(-pow(fd.x-x_0,2)/(2*s_0**2))
 
 fd.e[:] = e_ini[:]
 
-for _ in np.arange(0, 80*fd.dt, fd.dt):
+for t in np.arange(0, 100*fd.dt, fd.dt):
 
     fd.update()
 
+    #plt.plot(fd.x[int(1*fd.N/3):int(3*fd.N/4)], fd.sigma[int(1*fd.N/3):int(3*fd.N/4)], linestyle = '--', color = 'black', label = 'Obstaculo')
+
     plt.plot(fd.x, fd.e, linestyle = '-', marker = 's', markersize = 2, color = 'dodgerblue', label = 'E(x,t)')
     plt.plot(fd.x_d, fd.h, linestyle = '-', marker = 's', markersize = 2, color = 'orangered', label = 'H(x,t)')
-    plt.vlines(fd.w, -2, 2, color = 'black')
-    plt.vlines((fd.L-fd.w), -2, 2, color = 'black')
+    plt.vlines(fd.w, -2, 2, color = 'black', linestyles='--')
+    plt.vlines((fd.L-fd.w), -2, 2, color = 'black', linestyles='--')
     plt.xlabel('x (m)')
     plt.ylabel('Campos')
     plt.legend()
--- Trabajo/Run2D.py
+++ Trabajo/Run2D.py
@@ -1,21 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as patch
 
 import PML2D
+import OBSTACLE2D
 
 fd = PML2D.FDTD_PML2D()
 
-eps = 1.0
-mu = 1.0
-c = 1/np.sqrt(eps*mu)
+eps = 1.0                   # Permitividad eléctrica
+mu = 1.0                    # Permeabilidad magnética
+c = 1/np.sqrt(eps*mu)       # Velocidad de la onda
 
-x_ex, y_ex = np.meshgrid( fd.vx_d, fd.vy )
-x_ey, y_ey = np.meshgrid( fd.vx, fd.vy_d )
-x_h, y_h = np.meshgrid( fd.vx_d, fd.vy_d )
 
-x_0, s_0 = fd.L/2, 0.2 # Parámetros de la gaussiana
+x_ex, y_ex = np.meshgrid( fd.vx_d, fd.vy )    # Espacio en el que vive el campo Ex
+x_ey, y_ey = np.meshgrid( fd.vx, fd.vy_d )    # Espacio en el que vive el campo Ey
+x_h, y_h   = np.meshgrid( fd.vx_d, fd.vy_d )  # Espacio en el que vive el campo Hz
 
-fd.h_z[:, :] = np.exp(-(pow(x_h[:, :]-x_0, 2) + pow(y_h[:, :]-x_0, 2))/(2*s_0**2))
+x_0, y_0, s_0 = 1*fd.L/2, 1*fd.L/2, 0.2       # Parámetros de la gaussiana inicial
+
+
+fd.h_z = np.exp(-(pow(x_h-x_0, 2) + pow(y_h-x_0, 2))/(2*s_0**2))
 
 fd.b_z = mu*(fd.h_z)
 fd.e_x = np.zeros(x_ex.shape)
@@ -23,14 +27,31 @@ fd.e_y = np.zeros(x_ey.shape)
 fd.d_x = np.zeros(x_ex.shape)
 fd.d_y = np.zeros(x_ey.shape)
 
-for _ in np.arange(0, 120*fd.dt, fd.dt):
+counter = 0
+for _ in np.arange(0, 160*fd.dt, fd.dt):
 
     fd.h_z, fd.b_z = fd.update_h()
     fd.e_x[1:-1, :], fd.e_y[:, 1:-1], fd.d_x[:, :], fd.d_y[:, :] = fd.update_e()
 
-    plt.imshow(fd.h_z, cmap = 'viridis', interpolation='gaussian', vmin = -0.05, vmax = 0.4)
+    # PULSOS GAUSSIANOS
+    # counter += 1
+    # if counter%20 == 0:
+    #     fd.h_z[:, :] += np.exp(-(pow(x_h[:, :]-x_0, 2) + pow(y_h[:, :]-x_0, 2))/(2*s_0**2))
+    #     fd.b_z += mu*(fd.h_z)
+    
+    ax = plt.figure(1, figsize=(6,6))
+    plt.imshow(fd.h_z, cmap = 'viridis', interpolation = 'bilinear', vmin = -0.0, vmax = 0.4)
+
+    # PINTAR PELOTA
+    # ax = plt.gca()
+    # circle = patch.Circle( (3*fd.N/4, 1*fd.N/2), radius = 20, fill = False, ls = '--', color = 'black')
+    # ax.add_patch(circle)
+
+    plt.hlines(fd.N*9/10, fd.N*1/10, fd.N*9/10, color = 'black', linestyles='dashed')
+    plt.hlines(fd.N*1/10, fd.N*1/10, fd.N*9/10, color = 'black', linestyles='dashed')
+    plt.vlines(fd.N*9/10, fd.N*1/10, fd.N*9/10, color = 'black', linestyles='dashed')
+    plt.vlines(fd.N*1/10, fd.N*1/10, fd.N*9/10, color = 'black', linestyles='dashed')
+
     plt.axis('off')
-    cb = plt.colorbar()
     plt.pause(0.00001)
     plt.cla()
-    cb.remove()
